8da09a2e97cbd29aaa2e675b325c58d34a28143b
colin.voorneveld@student.tdsb.on.ca <colin.voorneveld@student.tdsb.on.ca>
2024-03-28 14:35:34 -0400

    in itial commit

---
+++ javd/src/App.java
@@ -0,0 +1,9 @@
+//This page was createc by colin voorneveld
+//On thurs, March 21st 2024
+//To "create a class for our first java app"
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
